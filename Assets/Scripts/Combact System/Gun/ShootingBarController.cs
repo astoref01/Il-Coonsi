8b957502e2ecf63fc56fb970710c20b7dbce6882
Michael <Michaelconsigli98@gmail.com>
2024-05-29 12:42:32 +0200

    aggiunto che se supera una soglia e premi il tasto destro del mouse il canvas torna a zero e ricomincia a caricarsi

--- Assets/Scripts/Combact System/Gun/ShootingBarController.cs
+++ Assets/Scripts/Combact System/Gun/ShootingBarController.cs
@@ -7,20 +7,26 @@ public class ShootingBarController : MonoBehaviour
     private GunScriptableObject gunScriptableObject;
     public Slider shootingBar;
     public int maxShots = 100;
-    private int currentShots;
+    private int resetThreshold = 50;
 
     void Start()
     {
         shootingBar.maxValue = maxShots;
         gunScriptableObject.i = 0;
     }
-    private void Update()
+
+    void Update()
     {
         UpdateShootingBar();
     }
+
     private void UpdateShootingBar()
     {
-
         shootingBar.value = gunScriptableObject.i;
+        if (shootingBar.value >= resetThreshold && Input.GetMouseButtonDown(1))
+        {
+            gunScriptableObject.i = 0;
+            shootingBar.value = 0;
+        }
     }
 }
